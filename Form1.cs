848d75cbb7b94e7a6bc1a90373ac8bce4725ffcd
usamamalik9 <um29393@gmail.com>
2019-11-24 00:35:03 +0500

    update code and functions

---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Tree temp = new Tree();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void LoadDictionary()
+        {
+            string line;
+
+            System.IO.StreamReader file = new System.IO.StreamReader(@"E:\New folder\4th semester\DSA\DSA\spell corector\spell corector\WORDS.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                temp.InsertData(line);
+            }
+
+            file.Close();
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadDictionary();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string a = textBox1.Text;
+            if (Search(temp.root, a)==true)
+            {
+                label4.Text = " Word Found";
+            }
+            else
+            {
+                label4.Text = "Word not Found";
+            }
+
+        }
+        public bool Search(Node a, string x)
+        {
+
+            if (a == null)
+                return false;
+            if (string.Compare(x, a.data) == 0)
+            {
+
+                return true;
+
+            }
+
+            if (string.Compare(x, a.data) < 0)
+                return Search(a.left, x);
+            else
+                return Search(a.right, x);
+        }
+    }
+}
--- node.cs
+++ node.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,22 +8,38 @@ namespace WindowsFormsApp1
 {
     public class Node
     {
-        public class Node
+        public string data { get; set; }
+        public Node left { get; set; }
+        public Node right { get; set; }
+
+        public Node()
         {
-            public string data { get; set; }
-            public Node left { get; set; }
-            public Node right { get; set; }
-
-            public Node()
-            {
-                data = "";
-                left = null;
-                right = null;
-            }
-            public Node(string s)
-            {
-                this.data = s;
-            }
-
+            data = "";
+            left = null;
+            right = null;
         }
+        public Node(string s)
+        {
+            this.data = s;
+        }
+
+    }
+    public class Tree
+    {
+        public Node root;
+        public Tree()
+        {
+            root = null;
+        }
+        public void InsertData(string data)
+        {
+            if (root == null)
+            {
+                root = new Node(data);
+                return;
+            }
+            Insertatchild(root, new Node(data));
+        }
+
+    }
 }
